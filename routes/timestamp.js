a15cb4eb1e91433df53f2abadceabbc32fd80521
RitaLmprt <rita.lempert@gmail.com>
2017-08-24 13:05:07 +0300

    Update timestamp.js

--- routes/timestamp.js
+++ routes/timestamp.js
@@ -4,19 +4,18 @@ var router = express.Router();
 var moment = require('moment');
 
 var getTimeObj = function(dateStr) {
-var timeObj = { unix: null, natural: null};
-var date = new Date(dateStr);
-
+    var timeObj = { unix: null, natural: null};
+    var date = new Date(dateStr);
     // Unix format given if string is all digits
     if (/^\d+$/.test(dateStr)) {
-      var unixVal = Number.parseInt(dateStr);
-       timeObj.unix = unixVal;
-       timeObj.natural = moment.unix(unixVal).format("MMMM DD, YYYY");
+        var unixVal = Number.parseInt(dateStr);
+        timeObj.unix = unixVal;
+        timeObj.natural = moment.unix(unixVal).format("MMMM DD, YYYY")
     }
     // Natural format given
     else if (dateStr.split(" ").length === 3) {
-      timeObj.unix = date.getTime()/1000,
-      timeObj.natural = dateStr
+        timeObj.unix = date.getTime()/1000,
+        timeObj.natural = dateStr
     }
     return timeObj;
 };
